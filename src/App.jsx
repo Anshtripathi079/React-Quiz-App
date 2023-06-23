4712434475d00947a30caa7d9e087c833f56760e
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-06-23 12:22:04 +0530

    made quiz page and fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,49 @@ import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
   return (
     <BrowserRouter>
       <div className="app">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quiz" element={<Quiz />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                name={name}
+                setName={setName}
+                fetchQuestions={fetchQuestions}
+              />
+            }
+          />
+          <Route
+            path="/quiz"
+            element={
+              <Quiz
+                name={name}
+                questions={questions}
+                score={score}
+                setScore={setScore}
+                setQuestions={setQuestions}
+              />
+            }
+          />
           <Route path="/result" element={<Result />} />
         </Routes>
       </div>
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Quiz = () => {
-  return <div>Quiz</div>;
+const Quiz = ({ name, score, questions, setQuestions, setScore }) => {
+  const [options, setOptions] = useState();
+  const [currQues, setcurrQues] = useState(0);
+  useEffect(() => {
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currQues]?.correct_answer,
+          ...questions[currQues]?.incorrect_answers,
+        ])
+    );
+  }, [questions]);
+  const handleShuffle = (opt) => {
+    return opt.sort(() => Math.random() - 0.5);
+  };
+  return (
+    <div>
+      <span>Welcome to the app {name}</span>
+    </div>
+  );
 };
 
 export default Quiz;
